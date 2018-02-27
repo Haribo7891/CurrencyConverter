dc49dfc1dc48aeb708483df086c77108c9083dcd
Haribo7891 <harry.crank@me.com>
2018-02-27 10:35:28 +0000

    [TextInput] and [Buttons] created

--- app/index.js
+++ app/index.js
@@ -5,7 +5,10 @@ import Home from './screens/Home';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
-  $white: '#FFFFFF'
+  $white: '#FFFFFF',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
+  $lightGrey: '#F0F0F0'
 });
 
 export default () => <Home />;
