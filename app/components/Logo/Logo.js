899a6e16165eb423cbc84ece0f5340c5bdaf57ad
Haribo7891 <harry.crank@me.com>
2018-02-27 09:25:00 +0000

    [Logo] component created

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Image, Text, ImageBackground } from 'react-native';
+
+import styles from './styles';
+
+const Logo = () => (
+  <View style={ styles.container }>
+    <ImageBackground resizeMode="contain" style={ styles.containerImage } source={ require('./images/background.png') }>
+      <Image resizeMode="contain" style={ styles.image } source={ require('./images/logo.png') }/>
+    </ImageBackground>
+    <Text style={ styles.text }>Currency Converter</Text>
+  </View>
+);
+
+export default Logo;
