cc4ede545c01a3a4e3dc8c11f7c5d4cbece31800
Haribo7891 <harry.crank@me.com>
2018-02-28 12:50:07 +0000

    [CurrencyList] screen created

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -13,4 +13,4 @@ EStyleSheet.build({
   //$outline: 1
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
